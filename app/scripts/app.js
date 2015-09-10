5b51da5ead2a906a4e0f4a8cffde1d2d6175aa9b
JOEL RAMSEY <joel.d.ramsey@gmail.com>
2015-09-09 21:08:28 -0600

    configured module with providers

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,8 @@
 var angularAppModule = angular.module('angularAppModule', ['ui-router']);
+
+angularAppModule.config(function ($stateprovider, $locationProvider) {
+    $locationProvider.html5Mode({
+        enabled: true,
+        requireBase: false
+    });
+});
