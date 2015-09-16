28a05b8c1b04a2818233d916c0d20d54671c33f6
JOEL RAMSEY <joel.d.ramsey@gmail.com>
2015-09-15 21:09:41 -0600

    WIP on services

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -34,10 +34,76 @@ blocJamsAngular.controller('Landing.controller', function ($scope) {
     $scope.welcome = 'Turn the music up!';
 });
 
-blocJamsAngular.controller('Album.controller', function ($scope) {
-    $scope.albumPicasso = albumPicasso;
-});
+blocJamsAngular.controller('Collection.controller', ['$scope', function ($scope) {
+    $scope.albums = [albumMarconi, albumPicasso, albumRamsey];
+}]);
 
-blocJamsAngular.controller('Collection.controller', function ($scope) {
-    $scope.albums = [albumPicasso, albumMarconi, albumRamsey];
+blocJamsAngular.controller('Album.controller', ['$scope', 'Player', function ($scope, Player) {
+    $scope.albumPicasso = albumPicasso;
+    $scope.play = function (song) {
+        Player.setSong($scope.album, song);
+
+    };
+    $scope.pause = function () {
+        Player.pause();
+    };
+}]);
+
+blocJamsAngular.service('Player', function () {
+    var getIndex = function (album, song) {
+        return album.songs.indexOf(song);
+    };
+
+    var currentSoundFile = null;
+
+    return {
+        isPlaying: false,
+        currentAlbum: null,
+        currentSong: null,
+        volume: 80,
+        play: function () {
+            currentSoundFile.play();
+            this.isPlaying = true;
+        },
+
+        pause: function () {
+            currentSoundFile.pause();
+            this.isPlaying = false;
+
+        },
+
+        setSong: function (album, song) {
+            if (currentSoundFile) {
+                currentSoundFile.stop();
+            }
+            this.currentAlbum = album;
+            this.currentSong = song;
+            currentSoundFile = new buzz.sound(song.audioUrl, {
+                formats: ['mp3'],
+                preload: true
+            });
+            this.play();
+            this.setVolume(this.volume);
+        },
+
+        next: function () {
+            var index = getIndex(this.currentAlbum, this.currentSong);
+            index++;
+            if (index >= this.currentAlbum.songs.length) {
+                index = 0;
+            }
+            var song = this.currentAlbum.songs[index];
+            this.setSong(this.currentAlbum, song);
+        },
+
+        previous: function () {
+            var index = getIndex(this.currentAlbum, this.currentSong);
+            index--;
+            if (index < 0) {
+                index = this.currentAlbum.songs.length - 1;
+            }
+            var song = this.currentAlbum.songs[index];
+            this.setSong(this.currentAlbum, song);
+        },
+    };
 });
