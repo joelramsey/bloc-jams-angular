71b9bc7c1ea3691f8823b1231cb08feb44796609
joelramsey <joel.d.ramsey@gmail.com>
2015-09-21 07:07:16 -0600

    serives wip. almost done

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -27,122 +27,125 @@ blocJamsAngular.controller('Landing.controller', ['$scope', function ($scope) {
     $scope.welcome = 'Turn the music up!';
 }]);
 
-blocJamsAngular.controller('Collection.controller', ['$scope', function ($scope) {
-    $scope.albums = [];
-    for (var x = 0; x < 12; x++) {
-        $scope.albums.push(albumPicasso);
+blocJamsAngular.controller('Collection.controller', ['$scope', function($scope) {
+    var albumsArray = [];
+    for (var i = 0; i < 8; i++) {
+        var currentAlbum = angular.copy(albumPicasso);
+        albumsArray.push(currentAlbum);
     }
+
+    $scope.albums = albumsArray;
 }]);
 
-blocJamsAngular.controller('Album.controller', ['$scope', 'Player', function ($scope, Player) {
-    $scope.album = albumPicasso;
-        Player.setCurrentAlbum(albumPicasso);
-    $scope.play = function () {
-        Player.play();
-    };
-    $scope.pause = function () {
-        Player.pause();
-    };
-    $scope.previous = function () {
-        Player.previous();
-    };
-    $scope.next = function () {
-        Player.next();
-    };
-    
-}]);
+blocJamsAngular.controller('Album.controller', ['$scope', 'Player', function($scope, Player) {
+    $scope.currentAlbum = Player.currentAlbum;
+    $scope.currentSoundFile = Player.currentSoundFile;
+    $scope.currentSongIndex = Player.currentSongIndex;
+    $scope.isPlaying = Player.playing;
+    Player.trackTime();
+    $scope.currentSongTime = Player.currentSongTime;
+    $scope.currentSongInAlbum = $scope.currentAlbum.songs[$scope.currentSongIndex];
 
-blocJamsAngular.service('Player', function () {
-    
-    var currentSoundFile = null;
-    return {
-        isPlaying: false,
-        currentAlbum: null,
-        currentSong: null,
-        currentSongFromAlbum: null,
-        volume: 80,
-        
-        play: function () {
-            if (currentSoundFile === null) {
-                this.setSong();
-            }
-            currentSoundFile.play();
-            this.isPlaying = true;
-        },
-        pause: function () {
-            currentSoundFile.pause();
-            this.isPlaying = false;
-        },
-        setVolume: function (value) {
-            if (currentSoundFile) {
-                currentSoundFile.setVolume(value);
-            }
-        },
+    $scope.playPause = function(songIndex){
+        Player.isPlaying(songIndex);
+        Player.setSong(songIndex);
 
-        setCurrentAlbum: function (album) {
-            this.currentAlbum = album;
-        },
-
-        setSong: function () {
-            if (currentSoundFile) {
-                currentSoundFile.stop();
-            }
-            
-            var song = 0;
-            
-            this.currentlyPlayingSongNumber = song;
-            this.currentSongFromAlbum = this.currentAlbum.songs[song];
-
-            currentSoundFile = new buzz.sound(this.currentSongFromAlbum.audioUrl, {
-                formats: ['mp3'],
-                preload: true
-            });
-        },
-        
-        getIndex: function (album, song) {
-            return album.songs.indexOf(song);    
-        },
-
-        previous: function () {
-            
-            var getLastSongNumber = function(index) {
-        
-            return index == 0 ? currentAlbum.songs.length : index;
-            };
-            var currentSongIndex = this.getIndex(this.currentAlbum, this.currentSongFromAlbum);
-            this.currentSongIndex--;
-            if(this.currentSongIndex < 0) {
-                currentSongIndex = currentAlbum.songs.length - 1;
-            }
-            
-            this.setSong(this.currentSongIndex + 1);
-            //this.currentSoundFile.play();
-            currentSoundFile.play()
-            
-        },
-        next: function () {
-            
-        var getLastSongNumber = function(index) {
-        
-            return index == 0 ? currentAlbum.songs.length : index;
-        };
-            var currentSongIndex = this.getIndex(this.currentAlbum, this.currentSongFromAlbum);
-            this.currentSongIndex++;
-            if(this.currentSongIndex >= this.currentAlbum.songs.length) {
-                currentSongIndex = 0;
-            }
-            
-            this.setSong(this.currentSongIndex + 1);
-            //this.currentSoundFile.play();
-            currentSoundFile.play()
-        },
-        
-        getTimePos: function () {
-            if (currentSoundFile) {
-                currentSoundFile.bind('timeupdate', function () {
-                    return this.getTime() / this.getDuration();
-                });
-            }
+        if(Player.playing){
+            Player.pause();
+        }
+        else{
+            Player.play();
         }
     };
+
+    $scope.nextSong = function() {
+        Player.nextSong();
+    };
+
+    $scope.previousSong = function() {
+        Player.previousSong();
+    };
+
+    $scope.playSong = function(){
+        $scope.isPlaying = Player.playing;
+        if(Player.playing){
+            Player.pause();
+        }
+        else{
+            Player.play();
+        }
+        $scope.isPlaying = Player.playing;
+    };
+}]);
+
+blocJamsAngular.factory('Player', function() {
+    return {
+        currentAlbum: albumPicasso,
+        currentSoundFile: null,
+        currentSongIndex: -1,
+        currentSongInAlbum: null,
+        currentVolume: 50,
+        currentSongTime: 0,
+        playing: false,
+        pause: function() {
+            this.playing = false;
+            this.paused = true;
+            this.currentSoundFile.pause();
+        },
+        play: function(){
+            this.playing = true;
+            this.paused = false;
+            this.currentSoundFile.play();
+        },
+        nextSong: function(){
+            this.currentSongIndex ++;
+            if(this.currentSongIndex === this.currentAlbum.songs.length){
+                this.currentSongIndex = 0;
+            }
+            this.setSong(this.currentSongIndex);
+            this.play();
+        },
+        previousSong: function(){
+            this.currentSongIndex --;
+            if(this.currentSongIndex === -1){
+                this.currentSongIndex = this.currentAlbum.songs.length -1;
+            }
+            this.setSong(this.currentSongIndex);
+            this.play();
+        },
+        setSong: function(songIndex){
+            if (this.currentSoundFile) {
+                this.currentSoundFile.stop();
+            }
+
+            this.currentSongIndex = songIndex;
+            this.currentSoundFile = new buzz.sound(albumPicasso.songs[songIndex].audioUrl, {
+                formats: [ 'mp3' ],
+                preload: true
+            });
+            this.setVolume(this.currentVolume);
+        },
+        isPlaying: function(songIndex){
+            if(this.currentSongIndex === songIndex && this.paused === false) {
+                this.playing = true;
+            }
+            else
+                this.playing = false;
+        },
+        setVolume: function(volume) {
+            if (this.currentSoundFile) {
+                this.currentSoundFile.setVolume(volume);
+            }
+        },
+        setTime: function(time) {
+            if (this.currentSoundFile) {
+                this.currentSoundFile.setTime(time);
+            }
+        },
+        trackTime: function() {
+            if (this.currentSoundFile) {
+                this.currentSongTime = this.currentSoundFile.getTime();
+            }
+        }
+    }
 });
